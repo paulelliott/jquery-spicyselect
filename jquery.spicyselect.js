3d863c4c6fe9adeb844f8eb991c5de7cd8c60381
Paul Elliott <paul@Daedalus.local>
2009-10-29 13:43:51 -0400

    Initial commit of source code

---
+++ jquery.spicyselect.js
@@ -0,0 +1,109 @@
+/*
+ * SpicySelect jQuery Plugin
+ *
+ * http://github.com/paulelliott/jquery-spicyselect
+ *
+ * Copyright (c) 2009 Paul Elliott (paul@codingfrontier.com)
+ *
+ * Based on the jListbox jQuery plugin by Giovanni Casassa (senamion.com - senamion.it)
+ *
+ * Dual licensed under the MIT (MIT-LICENSE.txt) and GPL (GPL-LICENSE.txt) licenses.
+ */
+(function($) {
+  $.fn.extend({
+    spicyselect: function(user_settings) {
+      //Override the default settings with the user settings
+      var settings = $.extend({
+        defaultText: "",
+        animate: true
+      }, user_settings);
+
+      //Iterate through all the selects being masked
+      return this.each(function() {
+        //Get and hide the select box
+        var selectBox = $(this).hide();
+        var selectOptions = $("> *", this);
+
+        //Create and store the id for the masking ol
+        var maskId = selectBox.attr('id') + '_mask';
+        selectBox.data("mask_id", maskId);
+
+        //Insert the basic structure for the mask into the DOM right after the select
+        selectBox.after("<div id='" + maskId + "'><a></a><ol></ol></div>");
+        var selectMask = $("#" + maskId).addClass("spicyselect").addClass(selectBox.attr("class"));
+        selectMask.data("select_id", selectBox.attr("id"));
+        var label = selectMask.find("> a");
+
+        //If the first option has no value, assume it to be the default.
+        if (selectOptions.length && !selectOptions[0].value) {
+          settings.defaultText = selectOptions[0].text;
+          selectOptions = selectOptions.slice(1);
+        }
+        label.text(settings.defaultText);
+
+        //Iterate over the children of the select
+        var options = selectMask.find("ol").hide();
+        selectOptions.each(function() {
+          var option = $(this);
+
+          //For an optgroup...
+          if (option.is("optgroup")) {
+            //Put the optgroup into a new ol
+            var optgroup = $("<ol></ol>");
+            optgroup.append(createLi(option));
+
+            //Iterate over the options contained within and put them in the ol
+            $("option", option[0]).each(function() {
+              optgroup.append(createLi($(this)));
+              $(this).is(":selected") && label.text(this.text);
+            });
+
+            options.append(optgroup);
+          } else {
+            if (!this.value) {
+              //A valueless option is considered to be the default value
+              settings.defaultText = this.text;
+            } else {
+              //Write an option with a blue into the ol
+              options.append(createLi(option));
+            }
+          }
+
+          //If the option was selected, use its label for display
+          option.is(":selected") && label.text(option.text());
+        });
+
+        //Toggle the the ol if the display anchor is clicked
+        selectMask.find("> a").click(function(){
+          var options = $(this).closest("div").find("ol");
+          settings.animate && options.slideToggle("fast") || options.toggle();
+          return false;
+        });
+
+        //When the user selects an option from the list
+        selectMask.find("ol li").click(function() {
+          if ($(this).is(".optgroup_label")) return false;
+          var mask = $(this).closest("div");
+          //Get the original select
+          $('#' + mask.data('select_id')).val($(this).data("value")).change();
+          mask.find("a:first").text($(this).text());
+          settings.animate && mask.find("> ol").slideUp("fast") || mask.find("> ol").hide();
+        });
+
+        //If the user clicks off the select, hide it
+        $("body").click(function(){
+          settings.animate && $(".spicyselect > ol").slideUp("fast") || $(".spicyselect > ol").hide();
+        });
+      });
+    }
+  });
+
+  //Create an li for the mask from an option or optgroup
+  function createLi(option) {
+    var text = option.is("optgroup") ? option.attr("label") : option.text();
+    var li = $("<li>" + text + "</li>").addClass(option.attr('class'));
+    option.is("optgroup") && li.addClass("optgroup_label");
+    li.data('value', option.val());
+    return li;
+  }
+})(jQuery);
---
+++ spec/jquery.spicyselect.spec.js
@@ -0,0 +1,111 @@
+Screw.Unit(function() {
+  describe("jquery-spicyselect", function() {
+    //Clean up the spicyselect after each run.
+    after(function() {
+      $("select").each(function() {
+        getMask($(this)).remove();
+        $(this).removeData("mask_id");
+      });
+    });
+
+    describe('when applied with the default options', function() {
+      before(function() { $("select").spicyselect({animate:false}); });
+
+      it("should have a data element with the id of the mask", function() {
+        expect($("select").data('mask_id')).to(be_true);
+      });
+      it("should have a mask present in the dom", function() {
+        expect(getMask($("select")).length).to(equal, 1);
+      });
+      it("should have a mask with the correct number of items in the drop down", function() {
+        expect(getMask($("select")).find("li").length).to(equal, 6);
+      });
+      it("should not have the valueless option", function() {
+        expect(getMask($("select")).find("li:contains(Valueless Option)").length).to(equal, 0);
+      });
+      it("should have the correct options", function() {
+        var mask = getMask($("select"));
+        expect(mask.find("li:eq(0)").text()).to(equal, "Basic Option");
+        expect(mask.find("li:eq(1)").text()).to(equal, "Awesome Option");
+        expect(mask.find("li:eq(1)").data('value')).to(equal, "awesome");
+        expect(mask.find("li:eq(2)").text()).to(equal, "Classy Option");
+        expect(mask.find("li:eq(2)").hasClass('classy')).to(be_true);
+      });
+      it("should have the correct default value", function() {
+        expect(getMask($("select")).find("> a").text()).to(equal, "Valueless Option");
+      });
+      it("should have a nested ol for the option group", function() {
+        expect(getMask($("select")).find("> ol").length).to(equal, 1);
+      });
+      it("should have the proper label on the option group", function() {
+        expect(getMask($("select")).find("> ol > ol li.optgroup_label").length).to(equal, 1);
+      });
+      it("should not have visible options", function() {
+        expect(getMask($("select")).find("> ol").is(":hidden")).to(be_true);
+      });
+    });
+
+    describe("when the select has a class attribute", function() {
+      before(function() { $("select").addClass("someclass").spicyselect({animate:false}); });
+      after(function() { $("select").removeClass("someclass"); });
+
+      it("should have the same class on the mask", function() {
+        expect(getMask($("select")).hasClass("someclass")).to(be_true);
+      });
+    });
+
+    describe("when the anchor is clicked", function() {
+      before(function() {
+        $("select").spicyselect({animate:false});
+        getMask($("select")).find("a").click();
+      });
+
+      it("should show the drop down", function() {
+        expect(getMask($("select")).find("> ol").is(":visible")).to(be_true);
+      });
+    });
+
+    describe("when an option is clicked", function() {
+      before(function() {
+        $("select").spicyselect({animate:false});
+        var mask = getMask($("select"));
+
+        //show the options
+        mask.find("a").click();
+
+        //click on the Classy Option
+        mask.find("li.classy").click();
+      });
+
+      it("should show the option text", function() {
+        expect(getMask($("select")).find("a").text()).to(match, getMask($("select")).find("li.classy").text());
+      });
+      it("should set the value of the underlying select", function() {
+        expect($("select").val()).to(match, getMask($("select")).find("li.classy").data("value"));
+      });
+      it("should hide the options", function() {
+        expect(getMask($("select")).find("> ol").is(":hidden")).to(be_true);
+      });
+    });
+
+    describe("when an optgroup label is clicked", function() {
+      before(function() {
+        $("select").spicyselect({animate:false});
+        var mask = getMask($("select"));
+
+        //show the options
+        mask.find("a").click();
+
+        //click on the optgroup label
+        mask.find("li.optgroup_label").click();
+      });
+
+      it("should not do anything", function() {
+        expect(getMask($("select")).find("> ol").is(":visible")).to(be_true);
+      });
+    });
+  });
+  function getMask(select) {
+    return $("#" + select.data("mask_id"));
+  }
+});
