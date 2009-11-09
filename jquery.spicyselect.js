6ff10475cf45a31772022780254f7202a440b7cf
Paul Elliott <paul@codingfrontier.com>
2009-11-09 17:22:48 -0500

    Down arrow, up arrow, space, enter, escape keys

--- jquery.spicyselect.js
+++ jquery.spicyselect.js
@@ -37,11 +37,51 @@
 
         //Insert the basic structure for the mask into the DOM right after the select
         selectBox.after("<div id='" + maskId + "'><a></a><ol></ol></div>");
-        var selectMask = $("#" + maskId).addClass("spicyselect").addClass(selectBox.attr("class")).focus(function(e) {
+        var selectMask = $("#" + maskId).addClass("spicyselect").addClass(selectBox.attr("class")).focus(function() {
           selectMask.addClass("focus");
         });
         selectMask.data("select_id", selectBox.attr("id"));
         var label = selectMask.find("> a");
+
+        //Handle the keypress events
+        selectBox.keydown(function(e) {
+          //If the keypress was not ctrl, alt, or command and not a tab or enter, send it to the mask instead
+          if (e.which != 9 && !e.altKey && !e.ctrlKey && !e.metaKey) {
+            //if the down arrow was pressed, show the list
+            if (e.which == 40) { //if the down arrow was pressed
+              if (selectMask.find("> ol").is(":hidden")) {
+                showMask(selectMask, settings);
+              } else {
+                var current = selectMask.find(".current");
+                if (current.length == 0) { //If none are selected, select the first one.
+                  selectMask.find("li:first").addClass("current");
+                } else if (!selectMask.find("li:last").is(".current")) { //If the last one is selected, do nothing
+                  //If any other one is selected, move it down one.
+                  var currentIndex = selectMask.find("li").index(current);
+                  selectMask.find("li:eq(" + (currentIndex + 1) + ")").addClass("current");
+                  current.removeClass("current");
+                }
+              }
+            } else if (e.which == 38 && selectMask.find("> ol").is(":visible")) { //if the up arrow is pressed
+              //If the first one is selected, do nothing
+              //If any other one is selected, move it up one.
+              var current = selectMask.find(".current");
+              var currentIndex = selectMask.find("li").index(current);
+              selectMask.find("li:eq(" + (currentIndex - 1) + ")").addClass("current");
+              current.removeClass("current");
+            } else if (e.which == 27) { //if escape is pressed
+              hideMask(selectMask, settings);
+            } else if ((e.which == 13 || e.which == 32) && selectMask.find("> ol").is(":visible")) { //enter or spacebar is pressed
+              //select the current option
+              selectOption(selectMask);
+              hideMask(selectMask, settings);
+            } else if (e.which == 13) {
+              //allow enter to pass through to the form if the the options are not visible.
+            }
+
+            return false;
+          }
+        });
 
         //If the first option has no value, assume it to be the default.
         if (selectOptions.length && !selectOptions[0].value) {
@@ -91,10 +131,9 @@
         //When the user selects an option from the list
         selectMask.find("ol li").click(function() {
           if ($(this).is(".optgroup_label")) return false;
-          var mask = $(this).closest("div");
+          var mask = $(this).addClass("current").closest("div");
           //Get the original select
-          $('#' + mask.data('select_id')).val($(this).data("value")).change();
-          mask.find("a:first").text($(this).text());
+          selectOption(mask);
           hideMask(mask, settings);
         });
 
@@ -109,17 +148,26 @@
   function hideMask(mask, settings) {
     settings.animate && mask.find("> ol").slideUp("fast") || mask.find("> ol").hide();
     mask.removeClass("focus");
+    mask.find(".current").removeClass("current");
   }
 
   function showMask(mask, settings) {
-    settings.animate && mask.find("> ol").slideToggle("fast") || mask.find("> ol").toggle();
+    settings.animate && mask.find("> ol").slideDown("fast") || mask.find("> ol").show();
     mask.addClass("focus");
+  }
+
+  function selectOption(mask) {
+    var option = mask.find(".current");
+    if (option.length > 0) {
+      mask.find("a:first").text(option.text());
+      $("#" + mask.data("select_id")).val(option.data("value")).change();
+    }
   }
 
   //Create an li for the mask from an option or optgroup
   function createLi(option) {
     var text = option.is("optgroup") ? option.attr("label") : option.text();
-    var li = $("<li>" + text + "</li>").addClass(option.attr('class'));
+    var li = $("<li>" + text + "</li>").addClass(option.attr('class')).attr("style", option.attr('style'));
     option.is("optgroup") && li.addClass("optgroup_label");
     li.data('value', option.val());
     return li;
